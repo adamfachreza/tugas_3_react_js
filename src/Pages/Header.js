08d16198f2b7a1bdeaed00e018175074ec8a7f0f
Utaa <adamfachreza15@gmail.com>
2022-12-28 23:40:50 +0700

    tugas 3 selesai

---
+++ src/Pages/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Header extends Component{
+    render(){
+        return(
+            <div>
+                <center>
+                <a href="/">Menu |</a>
+                <a href="/">Produk |</a>
+                <a href="/">Kontak |</a>
+                <a href="/">Tentang Kami |</a>
+                </center>
+                
+            </div>
+        );
+    }
+}
+
+export default Header;
